699db92b4e51a9402fbd115307e226a333da1c05
eul94458 <62548460+eul94458@users.noreply.github.com>
2020-12-08 11:34:14 +0900

    Update ProgramPart2.7_LocalChangez.py

--- 500mPOP/ProgramPart2.7_LocalChangez.py
+++ 500mPOP/ProgramPart2.7_LocalChangez.py
@@ -1,4 +1,4 @@
-print("#####################\nPart 2.6 program START.\n#####################")
+print("#####################\nPart 2.7 program START.\n#####################")
 import metadata
 from PIL import Image, ImageDraw
 import numpy
@@ -14,89 +14,63 @@ def call_populationmap(year):
     return matrix
 
 def defferentiate(universe):
-    for n in range(len(universe)-1):
-        before = universe[n]
-        after = universe[n+1]
 
-        matrixSlopeIncrease = [[0 for i in range(5120)] for j in range(6240)]
-        matrixSlopeDecrease = [[0 for i in range(5120)] for j in range(6240)]
-        matrixZ = [[0 for i in range(5120)] for j in range(6240)]
-        matrixNewCells = [[0 for i in range(5120)] for j in range(6240)]
+    matrixLocalChange = []
+    matrixLocalZ = []
+    matrixLocalStddev = [[0 for i in range(5120)] for j in range(6240)]
 
-        maxium = 0
-        minium = 0
-        
-        PlusAll = []
-        MinusAll = []
-        
-        for rows in range(6240):
-            for fields in range(5120):
-                condition1 = int(after[rows][fields])==0 and int(before[rows][fields])==0
-                if condition1 == False :
-                    if int(before[rows][fields]) == 0:
-                        matrixNewCells[rows][fields] = int(after[rows][fields])
-                    
-                    elif int(before[rows][fields]) != 0:
-                        different = int(after[rows][fields])-int(before[rows][fields])
-                        slope = different / int(before[rows][fields])
-                        if slope > 0:
-                            matrixSlopeIncrease[rows][fields] = slope
-                            PlusAll.append(slope)                        
-                        elif slope < 0:
-                            matrixSlopeDecrease[rows][fields] = slope
-                            MinusAll.append(slope)                              
+    for rows in range(6240):
+        for fields in range(5120):
+            condition1 = int(universe[1][rows][fields]) != 0
+            condition2 = int(universe[2][rows][fields]) != 0
+            condition3 = int(universe[3][rows][fields]) != 0
+            condition4 = int(universe[4][rows][fields]) != 0
+            conditionsum = condition1 or condition2 or condition3 or condition4
+            if conditionsum == True:
+                CellYX = str(rows) + "_" + str(fields)
+                changelist = []
 
-                        if slope > maxium:
-                            maxium = slope
-                        if slope < minium:
-                            minium = slope
-                        
-        PlusMean = sum(PlusAll)/len(PlusAll)
-        MinusMean = sum(MinusAll)/len(MinusAll)
-
-        PlusVar = []
-        for rows in matrixSlopeIncrease:
-            for fields in rows:
-                varient = (fields - PlusMean)**2
-                PlusVar.append(varient)
-        PlusStddev = (sum(PlusVar)/len(PlusVar))**(1/2)    
-        
-        MinusVar = []       
-        for rows in matrixSlopeDecrease:
-            for fields in rows:
-                varient = (fields - MinusMean)**2
-                MinusVar.append(varient)
-        MinusStddev = (sum(MinusVar)/len(MinusVar))**(1/2)
-        
-        for rows in range(6240):
-            for fields in range(5120):
-                if matrixSlopeIncrease[rows][fields] != 0 and matrixSlopeDecrease[rows][fields] == 0:
-                    zscore = (matrixSlopeIncrease[rows][fields] - PlusMean)/PlusStddev
-                    if zscore > 0:
-                        matrixZ[rows][fields] = zscore            
+                for n in range(len(universe)-1):
+                    before = universe[n][rows][fields]
+                    after = universe[n+1][rows][fields]
+                    pplchange = int(after)-int(before)
+                    changelist.append(pplchange)
                 
-                elif matrixSlopeDecrease[rows][fields] != 0 and matrixSlopeIncrease[rows][fields] == 0: 
-                    zscore = (matrixSlopeDecrease[rows][fields] - MinusMean)/MinusStddev
-                    if zscore < 0:
-                        matrixZ[rows][fields] = zscore
+                print(changelist)
+                changemean = sum(changelist) / len(changelist)
+                changevariantlist = []
+                for n in changelist:
+                    variant = (n - changemean)**2
+                    changevariantlist.append(variant)
+                samplestddev = sum(changevariantlist) / (len(changelist)-1)
 
-        print("max=",maxium,"+mean=",PlusMean,"+ddev=",PlusStddev, "min=",minium,"-mean=",MinusMean,"-stddev=",MinusStddev)
-        
-        Prefix = str(n+1)
-        
-        NameP = Prefix + "plusslope"
-        #drawmap(matrixSlopeIncrease,NameP,2)
-        
-        NameM = Prefix + "minusslope"
-        #drawmap(matrixSlopeDecrease,NameM,4)
-        
-        NameZ = Prefix + "zscore"
-        drawmap(matrixZ,NameZ,5)
-        
-        NameN = Prefix + "newcell"
-        #drawmap(matrixNewCells,NameN,1)
-
+                changezscore = []
+                for n in changelist:
+                    if samplestddev != 0:
+                        zscore = (n - changemean) / samplestddev
+                        changezscore.append(zscore)
+                    else:
+                        changezscore.append(0)
+                        
+                matrixLocalStddev[rows][fields] = samplestddev
+                changelist.insert(0,CellYX)
+                changezscore.insert(0,CellYX)
+                matrixLocalChange.append(changelist)
+                matrixLocalZ.append(changezscore)
     
+    
+    filenameChange = "data/outputdata/step2_populationdifference/LocalChange.csv"
+    writeout(matrixLocalChange,filenameChange)
+    
+    filenameZ = "data/outputdata/step2_populationdifference/LocalZ.csv"
+    writeout(matrixLocalZ,filenameZ)
+    
+    filenameStddev = "LocalStddec"
+    drawmap(matrixLocalStddev,filenameStddev,2)
+    
+    
+
+
 def drawmap(matx,c,para):
     temp = numpy.array(matx)
     im = Image.fromarray(temp,"RGB")
@@ -111,37 +85,37 @@ def drawmap(matx,c,para):
                 else:
                     pix[x,y] = (int(round(255-(255)/(1+6.96875*math.e**(-(0.0030937483*temp[y,x]))))),
                                 int(round(255-(255)/(1+6.96875*math.e**(-(0.0030937483*temp[y,x]))))),
-                                int(round(255-(255)/(1+6.96875*math.e**(-(0.0030937483*temp[y,x]))))))               
+                                int(round(255-(255)/(1+6.96875*math.e**(-(0.0030937483*temp[y,x]))))))
             elif para == 2:
                 if temp[y,x] == 0:
                     pix[x,y] = (255,255,255)
                 else:
                     pix[x,y] = (int(round(255-(255)/(1+6.96875*math.e**(-(0.01933592691*temp[y,x]))))),
                                 int(round(255-(255)/(1+6.96875*math.e**(-(0.01933592691*temp[y,x]))))),
-                                int(round(255-(255)/(1+6.96875*math.e**(-(0.01933592691*temp[y,x]))))))                 
+                                int(round(255-(255)/(1+6.96875*math.e**(-(0.01933592691*temp[y,x]))))))
             elif para == 3:
                 if temp[y,x] == 0:
                     pix[x,y] = (255,255,255)
                 elif -2 <= temp[y,x] <= 2 and temp[y,x] != 0:
-                    pix[x,y] = (128,128,128)   
+                    pix[x,y] = (128,128,128)
                 elif temp[y,x] < -2:
-                    pix[x,y] = (223,223,223)    
+                    pix[x,y] = (223,223,223)
                 elif 2 < temp[y,x]:
-                    pix[x,y] = (0,0,0)               
+                    pix[x,y] = (0,0,0)
             elif para == 4:
                 if temp[y,x] == 0:
                     pix[x,y] = (255,255,255)
                 else:
                     pix[x,y] = (int(round(255-(255)/(1+6.96875*math.e**(-(3.867185382*temp[y,x]))))),
                                 int(round(255-(255)/(1+6.96875*math.e**(-(3.867185382*temp[y,x]))))),
-                                int(round(255-(255)/(1+6.96875*math.e**(-(3.867185382*temp[y,x]))))))  
+                                int(round(255-(255)/(1+6.96875*math.e**(-(3.867185382*temp[y,x]))))))
             elif para == 5:
                 if temp[y,x] == 0:
                     pix[x,y] = (128,128,128)
                 else:
                     pix[x,y] = (int(round(255-(255)/(1+6.96875*math.e**(-(0.6445308971*(temp[y,x])+3))))),
                                 int(round(255-(255)/(1+6.96875*math.e**(-(0.6445308971*(temp[y,x])+3))))),
-                                int(round(255-(255)/(1+6.96875*math.e**(-(0.6445308971*(temp[y,x])+3))))))             
+                                int(round(255-(255)/(1+6.96875*math.e**(-(0.6445308971*(temp[y,x])+3))))))
 
             #logistic-0009
             #g(x)=255-(255)/(1+6.96875e^(-(0.0009*x)))
@@ -150,14 +124,14 @@ def drawmap(matx,c,para):
             #logistic-0.0030937483
             #h(x)=255-(255)/(1+6.96875e^(-(0.0030937483*x)))
             #h(0)=32 , b = 0.0030937483 , h(625)=128
-            
+
             #logistic-0.01933592691
             #m(x)=255-(255)/(1+6.96875e^(-(0.01933592691*x)))
             #m(0)=32 , b = 0.01933592691 , m(100)=128
-            
+
             #logistic-0.01933592691
             #m(x)=255-(255)/(1+6.96875e^(-(3.867185382*x)))
-            #m(0)=32 , b = 0.01933592691 , m(0.5)=128            
+            #m(0)=32 , b = 0.01933592691 , m(0.5)=128
 
             #linear
             #f(x)=255-( (223/1000)*x +32)
@@ -189,7 +163,7 @@ if __name__ == "__main__":
 
         #elif query == "quit":
         #    break
-        
+
 #max= 610.5 +mean= 0.4259507742607847 +ddev= 0.4749627144919295 min= -1.0 -mean= -0.1950052426900056 -stddev= 0.19530622874674042
 
 #max= 1545.0 +mean= 0.8248890106968028 +ddev= 0.9064839304260386 min= -1.0 -mean= -0.39053747089385843 -stddev= 0.39001647456425587
@@ -197,4 +171,4 @@ if __name__ == "__main__":
 #max= 1136.0 +mean= 0.5141332834719601 +ddev= 0.5808755089575358 min= -1.0 -mean= -0.26359615679908094 -stddev= 0.26364790390178944
 
 #max= 1103.0 +mean= 0.553058258193743 +ddev= 0.6192574953280646 min= -1.0 -mean= -0.29360948190878305 -stddev= 0.29358297316042353
-    print("#####################\nPart 2.6 program END. \n#####################")
+    print("#####################\nPart 2.7 program END. \n#####################")
